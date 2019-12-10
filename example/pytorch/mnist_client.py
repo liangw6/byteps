c3f18c2cd0f6a95ad3440d10204b02f28cdad6d8
Arthur Liang <liangw6@cs.washington.edu>
2019-12-10 07:44:56 +0000

    adds mnist data and analysis, cleans up code

--- example/pytorch/mnist_client.py
+++ example/pytorch/mnist_client.py
@@ -5,19 +5,22 @@ import multiprocessing as mp
 from torchvision import datasets, transforms
 import torch
 import sys
+import argparse
 
 import inference_utils
-
 DATA_PATH = './MNIST/'
 WORKERS = ['localhost']
-PORT = 50011
+PORT = 50015
 
-BATCH_SIZE = 16 #64
-REQUEST_PER_CLIENT = 10 # 100
-
-# toy
-BATCH_SIZE = 8
-REQUEST_PER_CLIENT = 5
+parser = argparse.ArgumentParser(description='PyTorch MNIST real-time inference client example')
+parser.add_argument('output_file', metavar='output.pkl', help='pickle file name to save the final result')
+parser.add_argument('--batch-size', type=int, default=64, metavar='N',
+                    help='input batch size for training (default: 64)')
+parser.add_argument('--request-per-client', type=int, default=1000, metavar='N',
+                    help='number of requests for each client process (default: 1000)')
+parser.add_argument('--nclient', type=int, default=8, metavar='N',
+                    help='number of clients in total, each of which run on separate process (default: 8)')
+args = parser.parse_args()
 
 # transofrm applied by byteps train_mnist_byteps.py
 DATA_TRANSFORM = transforms.Compose([
@@ -47,25 +50,24 @@ def one_client(client_id):
     datasets.MNIST(DATA_PATH, train=False, transform=DATA_TRANSFORM,
                    download=True)
     
-    test_loader = torch.utils.data.DataLoader(test_dataset, batch_size=BATCH_SIZE)
+    test_loader = torch.utils.data.DataLoader(test_dataset, batch_size=args.batch_size, drop_last=True)
     
     # keep track of result
     request_cnt = 0
     accu = []
-    latency = [] 
+    latency = []
     for data, target in test_loader:
-        if request_cnt >= REQUEST_PER_CLIENT:
+        if request_cnt >= args.request_per_client:
             # only  do so many requests
             break
-
         worker_latency = torch.zeros(len(WORKERS))
-        worker_answers = torch.zeros([len(WORKERS), BATCH_SIZE])
+        worker_answers = torch.zeros([len(WORKERS), args.batch_size])
         for index, one_worker in enumerate(WORKERS):
             recv_data, process_latency = client_send_and_recv(one_worker, PORT, data)
             if recv_data is None:
                 print("received None from worker", index)
                 continue
-            assert(recv_data.shape == (BATCH_SIZE, ))
+            assert(recv_data.shape == (args.batch_size, ))
 
             worker_answers[index, :] = recv_data
             worker_latency[index] = process_latency
@@ -75,13 +77,13 @@ def one_client(client_id):
         # print("step 1")
         # ONLY FOR MNIST DATASET!!!!! converting to int avoids floating point comparison problem
         curr_accu = worker_concensus.int().eq(target.int()).sum().float() / target.numel()
-        print("accuracy", curr_accu)
+        # print("accuracy", curr_accu)
         curr_latency = worker_latency.sum().float() / worker_latency.numel()
-        print("latency", curr_latency)
+        # print("latency", curr_latency)
 
         sys.stdout.flush()
-        accu.append(curr_accu)
-        latency.append(curr_latency)
+        accu.append(curr_accu.item())
+        latency.append(curr_latency.item())
         request_cnt += 1
 
     print("Client", client_id, "cleaning up")
@@ -90,15 +92,13 @@ def one_client(client_id):
     return accu, latency
 
 if __name__ == "__main__":
-    if len(sys.argv) != 2:
-        print("usage: python mnist_client.py n_clients")
-        sys.exit()
-    n_clients = int(sys.argv[1])
+    pool = mp.Pool(processes=args.nclient)
+    results = pool.map(one_client, range(args.nclient))
 
-    pool = mp.Pool(processes=n_clients) 
-    results = pool.map(one_client, range(n_clients))
+    print("received final result")
 
-    print("final results", results)
+    with open(args.output_file, 'w') as output_file:
+        pickle.dump(results, output_file, protocol=pickle.HIGHEST_PROTOCOL)
     
 
 
